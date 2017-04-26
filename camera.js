3662bc99950ed7e6f4531bb66bd77ab210c971f2
Maximilian <max.ragmo@outlook.com>
2017-04-26 22:00:26 +0200

    upload

---
+++ camera.js
@@ -0,0 +1,61 @@
+function navCamera() {
+  //Camera position and rotation
+  this.x = 0;
+  this.y = 0;
+  this.z = 0;
+  this.xRotation = 0;
+  this.yRotation = 0;
+
+  //Properties for rotating camera with mouse
+  this.lastMouseX = width / 2;
+  this.lastMouseY = height / 2;
+  this.beganMouseY = 0;
+  this.beganMouseX = 0;
+
+  this.update = function() {
+    this.checkInput();
+
+    camera(this.x, this.y, this.z);
+    //rotateX(-PI/2 + (PI)  * (this.yRotation / height));
+    rotateY(-PI/2 + (PI)  * (this.xRotation / width));
+  }
+
+  this.checkInput = function() {
+    //key input
+    if(keyIsDown(87)) { // W
+      this.z -= 10;
+    }
+    if(keyIsDown(83)) { //S
+      this.z += 10;
+    }
+    if(keyIsDown(65)) { //A
+      this.x -= 10;
+    }
+    if(keyIsDown(68)) { //D
+      this.x += 10;
+    }
+
+    //Calculate mouse movement while pressed
+    this.xRotation = this.lastMouseX;
+    this.yRotation = this.lastMouseY;
+    if (mouseIsPressed) {
+      var deltaX = mouseX - this.beganMouseX;
+      this.xRotation += deltaX;
+      var deltaY = mouseY - this.beganMouseY;
+      this.yRotation += deltaY;
+    }
+  }
+
+  this.onMouseRelease = function() {
+    var deltaX = mouseX - this.beganMouseX;
+    var deltaY = mouseY - this.beganMouseY;
+    this.lastMouseX += deltaX;
+    this.lastMouseY += deltaY;
+  }
+
+  this.onMousePress = function() {
+    this.beganMouseX = mouseX;
+    this.beganMouseY = mouseY;
+  }
+
+}
---
+++ sketch.js
@@ -0,0 +1,54 @@
+var cam;
+var img = [];
+var carousel;
+
+function preload() {
+  img.push(loadImage('nature.jpg'));
+  img.push(loadImage('nature2.jpg'));
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+  cam = new navCamera();
+  carousel = new carousel();
+}
+
+function draw() {
+  background(200);
+  cam.update();
+  carousel.update();
+}
+
+function carousel() {
+  // setup 8 boxes in a caraousel
+
+  this.total = 8;
+  this.radius = 2000;
+
+  this.update = function() {
+    this.centerX = 0;
+    this.centerY = 0;
+    for (var i = 0; i < this.total; i++) {
+      var x = (Math.cos(i / this.total * TWO_PI) * this.radius),
+          z = (Math.sin(i / this.total * TWO_PI) * this.radius),
+          y = 0;
+
+      push();
+      translate(x, y, z);
+      fill(100, 50, (255 / 8) * i);
+      texture(img[i % 2]);
+      rotateY(-QUARTER_PI * (i + 2));
+      box(800, 800, 800);
+      pop();
+    }
+  }
+}
+
+function mouseReleased() {
+  cam.onMouseRelease();
+}
+
+function mousePressed() {
+  cam.onMousePress();
+}
